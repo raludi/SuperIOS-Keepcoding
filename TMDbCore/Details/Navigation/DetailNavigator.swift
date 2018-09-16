9522d5788421070ba79bc3d168154c795532e4fa
Rafael Lujan <rafael.lujan@outlook.com>
2018-09-16 14:26:12 +0200

    Project

---
+++ TMDbCore/Details/Navigation/DetailNavigator.swift
@@ -0,0 +1,14 @@
+//
+//  DetailNavigator.swift
+//  TMDbCore
+//
+//  Created by Guille Gonzalez on 07/10/2017.
+//  Copyright © 2017 Guille Gonzalez. All rights reserved.
+//
+
+import Foundation
+
+protocol DetailNavigator {
+	func navigateToShow(withIdentifier identifier: Int64)
+	func navigateToMovie(withIdentifier identifier: Int64)
+}
